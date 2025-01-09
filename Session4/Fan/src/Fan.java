58f2f5eb0c0fc8106dfbd92fc491bd584bb06b36
HaiNguyenBGG <huuhai97bg@gmail.com>
2025-01-09 15:32:30 +0700

    message

---
+++ Session4/Fan/src/Fan.java
@@ -0,0 +1,48 @@
+public class Fan {
+    public final int SLOW = 1;
+    public final int MEDIUM = 2;
+    public final int FAST = 3;
+
+    private int speed;
+    private boolean on;
+    private double radius;
+    private String color;
+
+    public Fan() {
+        speed = SLOW;
+        on = false;
+        radius = 5;
+        color = "blue";
+    }
+
+    public int getSLOW() {
+        return SLOW;
+    }
+    public int getMEDIUM() {
+        return MEDIUM;
+    }
+    public int getFAST() {
+        return FAST;
+    }
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    @Override
+    public String toString() {
+        return "Fan{" +
+                "speed=" + speed +
+                ", on=" + on +
+                ", radius=" + radius +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
