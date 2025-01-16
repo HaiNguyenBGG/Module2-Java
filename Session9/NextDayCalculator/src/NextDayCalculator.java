8ecd08c2d336c6ba3f6338e3057cfad5e549a415
HaiNguyenBGG <huuhai97bg@gmail.com>
2025-01-16 14:22:41 +0700

    refactor test NextDayCaculator End

--- Session9/NextDayCalculator/src/NextDayCalculator.java
+++ Session9/NextDayCalculator/src/NextDayCalculator.java
@@ -2,63 +2,54 @@ public class NextDayCalculator {
 
     public static final int LAST_DAY_OF_FULL_MONTH = 31;
     public static final int LAST_DAY_OF_MISS_MONTH = 30;
+    public static final int LAST_DAY_OF_FEBRUARY_NON_LEAP = 28;
+    public static final int LAST_DAY_OF_FEBRUARY_LEAP = 29;
     public static final String SLASH = "/";
     public static final int FIRST_DAY_OF_MONTH = 1;
 
     public static String nextDay(int day, int month, int year) {
-        return checkMonth(day, month, year);
+        return calculateNextDay(day, month, year);
     }
 
-    private static String checkMonth(int day, int month, int year) {
+    private static String calculateNextDay(int day, int month, int year) {
         switch (month) {
-            case 1:
-            case 3:
-            case 5:
-            case 7:
-            case 8:
-            case 10:
-            case 12:
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
                 if (day == LAST_DAY_OF_FULL_MONTH) {
-                    day = FIRST_DAY_OF_MONTH;
-                    if (month == 12) {
-                        month = 1;
-                        year++;
-                    } else {
-                        month++;
-                    }
-                } else {
-                    day++;
+                    return resetToFirstDay(day, month, year);
                 }
                 break;
-            case 4:
-            case 6:
-            case 9:
-            case 11:
+
+            case 4: case 6: case 9: case 11:
                 if (day == LAST_DAY_OF_MISS_MONTH) {
-                    day = FIRST_DAY_OF_MONTH;
-                    month++;
-                } else {
-                    day++;
+                    return resetToFirstDay(day, month, year);
                 }
                 break;
+
             case 2:
-                checkIsLeapYear(year);
-                if (day == 28) {
-                    day = FIRST_DAY_OF_MONTH;
-                    month++;
-                } else if (day == 29) {
-                    day = FIRST_DAY_OF_MONTH;
-                    month++;
-                } else {
-                    day++;
+                int lastDayOfFebruary = isLeapYear(year) ? LAST_DAY_OF_FEBRUARY_LEAP : LAST_DAY_OF_FEBRUARY_NON_LEAP;
+                if (day == lastDayOfFebruary) {
+                    return resetToFirstDay(day, month, year);
                 }
+                break;
+        }
+
+        // Default increment day
+        day++;
+        return day + SLASH + month + SLASH + year;
+    }
+
+    private static String resetToFirstDay(int day, int month, int year) {
+        day = FIRST_DAY_OF_MONTH;
+        if (month == 12) {
+            month = 1;
+            year++;
+        } else {
+            month++;
         }
         return day + SLASH + month + SLASH + year;
     }
 
-    public static void checkIsLeapYear(int year) {
-        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-            System.out.println("Leap year");
-        }
+    private static boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
     }
 }
