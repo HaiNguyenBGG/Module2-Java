f208f702b0ffa05ebe2f54f024372455d43e5d8e
HaiNguyenBGG <huuhai97bg@gmail.com>
2025-01-16 13:44:13 +0700

    pass test 30/4/2018

--- Session9/NextDayCalculator/src/NextDayCalculator.java
+++ Session9/NextDayCalculator/src/NextDayCalculator.java
@@ -1,8 +1,40 @@
 public class NextDayCalculator {
     public static String nextDay(int day, int month, int year) {
-        if (day == 31 ) {
-            return "1/" + (++month) + "/" + year;
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                if (day == 31) {
+                    day = 1;
+                    if (month == 12) {
+                        month = 1;
+                        year++;
+                    } else {
+                        month++;
+                    }
+                } else {
+                    day++;
+                }
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                if (day == 30) {
+                    day = 1;
+                    month++;
+                } else {
+                    day++;
+                }
+                break;
+            case 2:
+                day =28;
+                break;
         }
-        return (++day) + "/" + month + "/" + year;
+        return day + "/" + month + "/" + year;
     }
 }
